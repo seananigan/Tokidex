89d41f766899f33f2f9d400a32b2886ea34c6a27
Sean Dales <seandales@d207-023-176-052.wireless.sfu.ca>
2019-10-15 14:00:51 -0700

    back

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const PORT = process.env.PORT || 5000
 var app = express();
 
 const { Pool } = require('pg');
-var pool;
-pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,6 +15,20 @@ app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {res.render('pages/tokidex')});
+
+.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
 app.get('/add', (req,res) => { res.render('pages/add')});
 app.get('/users', (req,res) => {
   var getUsersQuery = `SELECT * FROM userstab`;
