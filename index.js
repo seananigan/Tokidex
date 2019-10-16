3d75b222a8d6cd07b83721b83ad2d7c064e55d24
Sean Dales <seandales@d207-023-176-052.wireless.sfu.ca>
2019-10-15 22:01:26 -0700

    features now

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var app = express();
 const { Pool } = require('pg');
 var pool;
 pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-  // connectionString: 'postgres://postgres:postgres@localhost/tokidexdb'
+  //connectionString: process.env.DATABASE_URL
+  connectionString: 'postgres://postgres:postgres@localhost/tokidexdb'
 });
 pool.connect();
 
@@ -18,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
 
-   var createTable = `CREATE TABLE IF NOT EXISTS public.tokidextable2 (name varchar(50), trainer varchar(50), height int, weight int, fire int, water int, electric int, 
+   var createTable = `CREATE TABLE IF NOT EXISTS public.tokidextable3 (toki_id serial, name varchar(50), trainer varchar(50), height int, weight int, fire int, water int, electric int, 
    fly int, fight int, ice int)`
   pool.query(createTable,(error, result) => {});
   {res.render('pages/tokidex')}
@@ -26,28 +26,41 @@ app.get('/', (req, res) => {
 
 app.get('/addform', (req,res) => { res.render('pages/addform')});
 
+app.get('/deleted', (req,res) => { res.render('pages/deleted')});
+
 app.get('/view', (req,res) => { res.render('pages/viewAll')});
 
 app.get('/viewAll', (req,res) => {
-  var viewQuery = `SELECT * FROM tokidextable2`;
+  var viewQuery = `SELECT * FROM tokidextable3`;
   console.log(viewQuery);
   pool.query(viewQuery, (error, result) => {
     if (error)
       res.end(error);
-    console.log(results);
+    
     var results = {'rows': result.rows };
+    console.log(results);
     
     res.render('pages/viewAll', results)
   });
 });
 
 app.get('/remove/:id', (req,res) => {
-  req.params.tid
-  var deleteQuery = DELETE FROM tokidextable2 WHERE id=${req.params.id};
+  var deleteQuery = `DELETE FROM tokidextable3 WHERE id=${req.params.id}`;
   pool.query(deleteQuery, (error, result) => {
       if (error)
           res.end(error);
-      res.render('pages/tokidex')
+  });
+  res.render('pages/deleted')
+});
+
+app.get('/info/:id', (req,res) => {
+  req.params.id
+  var idQuery = `SELECT * FROM tokidextable3 WHERE id=${req.params.id}`;
+  pool.query(idQuery, (error, result) => {
+      if (error)
+          res.end(error);
+      var results = {'rows': result.rows };
+      res.render('pages/info', results)
   });
 });
 
@@ -55,10 +68,11 @@ app.get('/remove/:id', (req,res) => {
 //   console.log(req.params.id);
 //   var userIDQuery = `SELECT * FROM userstab WHERE uid=${req.params.id}`;
 // });
+
 app.post('/submit', (req, res) => {
 
 
-  var insertQuery = `INSERT INTO tokidextable2(name, trainer, height, weight, fire, water, electric, fly, fight, ice) 
+  var insertQuery = `INSERT INTO tokidextable3(name, trainer, height, weight, fire, water, electric, fly, fight, ice) 
     VALUES ('${req.body.nameInput}', '${req.body.trainerInput}', '${req.body.heightInput}', '${req.body.weightInput}', '${req.body.fireInput}', 
     '${req.body.waterInput}', '${req.body.electricInput}', '${req.body.flyInput}', '${req.body.fightInput}', '${req.body.iceInput}')`;
     console.log(insertQuery);
